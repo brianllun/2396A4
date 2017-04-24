cd016c90936516324cabc52d7e965be37c971c2b
Cheater <lht031052@gmail.com>
2017-04-24 20:47:42 +0800

    Fail

--- ImageServer.java
+++ ImageServer.java
@@ -16,50 +16,56 @@ import javax.swing.*;
 import javax.swing.event.*;
 import javax.imageio.ImageIO;
 
-public class ImageServer {
-	static JFrame FMain;
-
-    public static void main(String[] args) throws Exception {
-        ImageServer iServer = new ImageServer();
-    	iServer.FMain = new JFrame("Image Server");
-    	iServer.FMain.setBounds(0,0,700,600);
-    	JFileChooser FChooser = new JFileChooser();
-    	int returnVal = FChooser.showOpenDialog(FMain);
-
-			Container fCPane = FMain.getContentPane();
-			JButton reloadButton = new JButton("Load another image");
-			fCPane.add(reloadButton,BorderLayout.SOUTH);
-
-    	if (returnVal == JFileChooser.APPROVE_OPTION) {
+public class ImageServer extends JPanel{
+    protected JFileChooser FChooser = new JFileChooser();
+    public ImageServer(){
+        int returnVal = FChooser.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION){
                 ImagePanel iPanel = new ImagePanel(FChooser.getSelectedFile());
-				fCPane.add(iPanel,BorderLayout.CENTER);
-    		FMain.setVisible(true);
-    		FMain.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    	} else {
-    		System.exit(0); 
-    	}
+                add(iPanel,BorderLayout.CENTER);
+        } else {
+            System.exit(0);
+        }
+        JButton reloadButton = new JButton("Load another image");
+        add(reloadButton,BorderLayout.SOUTH);
     }
 
-        public static class ImagePanel extends JPanel {
-        	private BufferedImage imageBuffer;
+    private static void showGUI() {
 
-        	ImagePanel(File f){
-                BufferedImage iBuffer_temp;
-                try
-				{
-                        iBuffer_temp = ImageIO.read(f);
-                }
-				catch(Exception e)
-				{
-                        javax.swing.JOptionPane.showMessageDialog(null, "Cannot load image "+ f.getName());
-                        iBuffer_temp = null;
-                }
-                imageBuffer = iBuffer_temp;
+        JFrame FMain = new JFrame("Image Server");
+        FMain.setDefaultCloseOperation(FMain.EXIT_ON_CLOSE);
 
-        	}
-        	protected void paintComponent(Graphics g) {
-        		super.paintComponent(g);
-        		g.drawImage(imageBuffer, 0, 0, this);
-        	}
-        }
+        ImageServer newContentPane = new ImageServer();
+
+        newContentPane.setOpaque(true);
+        FMain.setBounds(0,0,600,600);
+        FMain.getContentPane().add(newContentPane,BorderLayout.CENTER);
+        FMain.pack();
+        FMain.setVisible(true);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        showGUI();
+    }
+
+    public static class ImagePanel extends JPanel {
+    	private BufferedImage imageBuffer;
+    	ImagePanel(File f){
+            BufferedImage iBuffer_temp;
+            try{
+                    iBuffer_temp = ImageIO.read(f);
+            }
+			catch(Exception e){
+                    javax.swing.JOptionPane.showMessageDialog(null, "Cannot load image "+ f.getName());
+                    iBuffer_temp = null;
+            }
+            imageBuffer = iBuffer_temp;
+
+    	}
+    	protected void paintComponent(Graphics g) {
+    		super.paintComponent(g);
+    		g.drawImage(imageBuffer, 0, 0, 600, 600, null);
+    	}
+    }
 }
