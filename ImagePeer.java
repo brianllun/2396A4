574457f1d68290c1962c7938502915c68499f156
Cheater <lht031052@gmail.com>
2017-05-03 18:13:32 +0800

    Successfully change image

--- ImagePeer.java
+++ ImagePeer.java
@@ -1,34 +1,93 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
 import java.net.*;
+import java.nio.*;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.*;
+//import javax.swing.event.*;
+import javax.imageio.*;
 
-public class ImagePeer {
-
-		protected static JFrame FMain = new JFrame("Image Client");
-		protected static InetAddress IPAdd;
-
+public class ImagePeer extends JPanel{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 16476560113239447L;
+	protected ImagePanel iPanel;
+	protected JFrame FMain = new JFrame("Image Client");
+	protected InetAddress IPAdd;
+	protected BufferedImage imageBuffer;
+	{
+		iPanel = new ImagePanel();
+        iPanel.setPreferredSize(new Dimension(600, 600));
+        add(iPanel,BorderLayout.CENTER);
+	}
 	public static void main(String[] args) throws Exception {
 		// int max_connection = Integer.parseInt(args[0]);
-		// showGUI();
-		InetAddress IP=InetAddress.getLocalHost();
-		IPAdd = IPAdd.getByName(inputBox("Input your fucking Server IP!","IP.getHostAddress()"));
+
+		ImagePeer ipeer = new ImagePeer();
+		ipeer.go();
+	};
+	public ImageIcon fetchImage(byte[] b, InputStream is){
+		try{
+			int nbrRd = 0;
+			int nbrLeftToRead = b.length;
+			while(nbrLeftToRead > 0){
+				int readin =is.read(b, nbrRd, nbrLeftToRead);
+			 	if(readin < 0) break;
+			  	nbrRd += readin;
+			  	nbrLeftToRead -= readin;
+			  	System.out.println("Got part of image. read in=" + readin + ", byte left= " + nbrLeftToRead
+								+ " space available=" + is.available());
+			}
+			System.out.println("Got image. nbrToRead=" + b.length + ", nbrRd=" + nbrRd);
+			return (new ImageIcon(b));
+			// return (new ImageIcon(new ByteArrayInputStream(b)));
+		}catch (Exception ex){
+			ex.printStackTrace();
+			return(null);
+		}
+	}
+	public void go(){
+		JLabel jl = new JLabel();
+		showGUI();
+		try{
+			IPAdd = InetAddress.getByName(inputBox("Input your fucking Server IP!","IP.getHostAddress()"));
+		} catch (UnknownHostException ex){ex.printStackTrace();}
 		try{
 			Socket sock = new Socket(IPAdd.getHostAddress(), 8000);
+			InputStream in = sock.getInputStream();
+			infoBox("(Client)Connected!","");
+			byte[] sizeAr = new byte[4];
+			jl.setVisible(true);
+			jl.setPreferredSize(new Dimension(600,600));
+			iPanel.add(jl);
+			while (true){
+				try{
+					if(in.read(sizeAr) < 0){sock.close(); break;};
+					infoBox("looping","");
+					byte[] byteAr = new byte[ByteBuffer.wrap(sizeAr).asIntBuffer().get()];
+					ImageIcon tempImage = fetchImage(byteAr, in);
+					jl.setIcon(tempImage);
+
+
+					// JDialog dialog = new JDialog();
+					// dialog.setPreferredSize(new Dimension(600,600));
+					// JLabel label = new JLabel(tempImage);
+					// dialog.add( label );
+					// dialog.pack();
+					// dialog.setVisible(true);
+				}catch (IOException e){
+					infoBox("Connection Closed!","");
+					break;
+				}
+
+			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
-			infoBox("Fail","Fail");
+			infoBox("(Client)Connection Closed","Fail");
 		}
-	};
+	}
 	public static void infoBox(String infoMessage, String titleBar){
 	   JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
 	}
@@ -37,5 +96,36 @@ public class ImagePeer {
 		s = JOptionPane.showInputDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
 		return (s);
 	}
-	public void showGUI(){}
+	private void showGUI(){
+		FMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		FMain.getContentPane().add(this,BorderLayout.CENTER);
+		FMain.pack();
+		FMain.setVisible(true);
+	}
+	public static class ImagePanel extends JPanel {
+        /**
+		 * 
+		 */
+		private static final long serialVersionUID = 768818327301329884L;
+		private BufferedImage imageBuffer;
+		ImagePanel(){
+        }
+		public void setImage(BufferedImage f){
+			imageBuffer = f;
+		}
+        public void setImage(File f){
+            BufferedImage iBuffer_temp;
+            try{
+                    iBuffer_temp = ImageIO.read(f);
+            }
+            catch(Exception e){
+                iBuffer_temp = null;
+            }
+            imageBuffer = iBuffer_temp;
+        }
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(imageBuffer, 0, 0, 600, 600, null);
+        }
+    }
 }
--- ImageServer.java
+++ ImageServer.java
@@ -1,26 +1,26 @@
-import java.awt.event.*;
+
+
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.*;
-import java.io.BufferedReader;
-import java.io.File;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.net.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.*;
+import java.util.*;
 import java.util.HashSet;
-
+import javax.imageio.*;
 import javax.swing.*;
 import javax.swing.event.*;
-import javax.imageio.ImageIO;
-
-import java.util.*;
-import java.net.*;
-
 public class ImageServer extends JPanel{
+
     protected ImagePanel iPanel;
     protected JFileChooser FChooser = new JFileChooser();
-    protected static JFrame FMain = new JFrame("Image Server");
+    protected JFrame FMain = new JFrame("Image Server");
+    protected File IFile;
     private int max_connection;
     private static int peercount = 0;
     {
@@ -35,6 +35,7 @@ public class ImageServer extends JPanel{
                 getImage();
                 revalidate();
                 repaint();
+
             }
         });
         add(reloadButton,BorderLayout.SOUTH);
@@ -49,14 +50,18 @@ public class ImageServer extends JPanel{
     private void getImage(){
         int returnVal = FChooser.showOpenDialog(this);
         if (returnVal == JFileChooser.APPROVE_OPTION){
-                iPanel.setImage(FChooser.getSelectedFile());
+            IFile = FChooser.getSelectedFile();
+            try{
+                iPanel.setImage((BufferedImage) getScaledImage(ImageIO.read(IFile),600,600));
+            } catch (IOException e){
+                e.printStackTrace();
+            }
         } else {
             System.exit(0);
         }
     }
     private void showGUI() {
-        FMain.setDefaultCloseOperation(FMain.EXIT_ON_CLOSE);
-        FMain.setBounds(0,0,600,600);
+        FMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         FMain.getContentPane().add(this,BorderLayout.CENTER);
         FMain.pack();
         FMain.setVisible(true);
@@ -72,31 +77,43 @@ public class ImageServer extends JPanel{
         private BufferedImage imageBuffer;
         ImagePanel(){
         }
-        public void setImage(File f){
-            BufferedImage iBuffer_temp;
-            try{
-                    iBuffer_temp = ImageIO.read(f);
-            }
-            catch(Exception e){
-                iBuffer_temp = null;
-            }
-            imageBuffer = iBuffer_temp;
+        public void setImage(BufferedImage b){
+            imageBuffer = b;
         }
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             g.drawImage(imageBuffer, 0, 0, 600, 600, this);
         }
     }
+    private BufferedImage[][] getSplittedImage(BufferedImage b){
+        BufferedImage[][] biArray = new BufferedImage[20][20];
+        for (int i=0; i*30 < 600; i++){
+            for (int j=0; j*30 < 600; j++){
+                biArray[i][j] = b.getSubimage(i*30, j*30, 30, 30);
+            }
+        }
+        return biArray;
+    }
+    private Image getScaledImage(Image srcImg, int w, int h){
+        BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = resizedImg.createGraphics();
+
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(srcImg, 0, 0, w, h, null);
+        g2.dispose();
+
+        return resizedImg;
+    }
     public static void main(String[] args) throws Exception {
         ImageServer hehe = new ImageServer(new BorderLayout());
-        hehe.max_connection_set(Integer.parseInt(args[0]));
+//        hehe.max_connection_set(Integer.parseInt(args[0]));
         hehe.go();
     };
     public void go(){
         showGUI();
         listenClient();
     }
-    public static void listenClient(){
+    public void listenClient(){
         HashMap<String,Socket> ClientList = new HashMap<String,Socket>();
         ServerSocket serverSocket = null;
         Socket cHandleSocket = null;
@@ -110,7 +127,6 @@ public class ImageServer extends JPanel{
         while(true){
             try{
                     cHandleSocket = serverSocket.accept();
-                    infoBox("on9","on9");
                     ClientHandler cl = new ClientHandler(cHandleSocket);
                     peercount++;
                     cl.start();
@@ -119,41 +135,59 @@ public class ImageServer extends JPanel{
             }
         }
     }
-    public static class ClientHandler extends Thread{
+    public class ClientHandler extends Thread {
 
         private boolean socket_Accpeted = false;
         private Socket socket;
-        private BufferedReader in;
+        private File jf = IFile;
+        private String jfs = jf.getName();
         private OutputStream out;
+        private ByteArrayOutputStream baout = new ByteArrayOutputStream();
         public ClientHandler(Socket s){
             this.socket = s;
         }
-        public void run(){
-            OutputStream out = socket.getOutputStream();
-            JFileChooser jf = FChooser;
-            ByteArrayOutputStream baout = new ByteArrayOutputStream();
-            while(true){
-                infoBox("Client"+peercount,"");
-                if (!jf.equal(FChooser)){
-                    infoBox("Changed!","");
-                    BufferedImage bfImage = ImageIO.read(jf.getSelectedFile());
-                    ImageIO.write(bfImage, jf.substring(jf.lastIndexOf("."),jf.length()), baout);
-                    byte[] size = ByteBuffer.allocate(4).putInt(baout.size()).array();
-                    out.write(size);
-                    out.write(baout.toByteArray());
-                    out.flush();
-                }
-                try{
-                    this.sleep(5000);
-                } catch(InterruptedException e){
-                    e.printStackTrace();
-                }
-            }
+        public void sendImage() throws IOException{
+            jf = IFile;
+            jfs = jf.getName();
+            BufferedImage bfImage = (BufferedImage) getScaledImage(ImageIO.read(jf),600,600);
+            System.out.println("Image to BABuffer: "+ImageIO.write(bfImage, jfs.substring(jfs.lastIndexOf(".")+1,jfs.length()), baout));
+            System.out.print("File Size: "+baout.size());
+            byte[] size = ByteBuffer.allocate(4).putInt(baout.size()).array();
+            byte[] realbyte = baout.toByteArray();
+
+            BufferedImage[][] bfImageArray = getSplittedImage(bfImage);
+            // JDialog dialog = new JDialog();
+            // dialog.setPreferredSize(new Dimension(600,600));
+            // JLabel label = new JLabel(new ImageIcon(realbyte));
+            // dialog.add( label );
+            // dialog.pack();
+            // dialog.setVisible(true);
+
+            out.write(size);
+            out.flush();
+            out.write(realbyte);
+            out.flush();
+            baout.reset();
         }
-    }
-    public class OpenPort8000 implements Runnable{
-        public void run() {
-            infoBox("dllm","dllm");
+        public void run(){
+            try{
+                out = socket.getOutputStream();
+                sendImage();
+                while(true){
+                    infoBox("(Server) Connected Client"+peercount,"");
+                    if (!jf.equals(IFile)){
+                        infoBox("Picture changed!","");
+                        sendImage();
+                    }
+                    try{
+                        this.sleep(5000);
+                    } catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }
         }
     }
 }
